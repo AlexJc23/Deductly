30391f62c6578280f354467a19d685f07e714a93
Alex Carl <alexjcarl97@gmail.com>
2026-04-04 23:52:25 -0400

    backend structure begin

---
+++ backend/app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    app_name: str
+    debug: bool
+    database_url: str
+    secret_key: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+
+
+
+#  python -m uvicorn app.main:app --reload
